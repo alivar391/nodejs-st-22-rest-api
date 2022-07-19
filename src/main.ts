2e3602a947f46ac7e6cb757cf60ffb18cea8c1d9
alivar391 <patriot391@mail.ru>
2022-07-19 08:48:38 +0300

    feat: add validationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(PORT);
 }
 bootstrap();
