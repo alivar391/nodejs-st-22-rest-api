54023b578c835687a0984b7386029f6cf4cb4b49
alivar391 <patriot391@mail.ru>
2022-08-12 09:06:01 +0300

    feat: add error logging in controllers

--- src/exception-filter/all-exception.filter.ts
+++ src/exception-filter/all-exception.filter.ts
@@ -6,12 +6,15 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
+import { logger } from 'src/logger/logger';
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
   catch(exception: unknown, host: ArgumentsHost): void {
     const { httpAdapter } = this.httpAdapterHost;
     const ctx = host.switchToHttp();
+    const req = ctx.getRequest();
+    const res = ctx.getResponse();
     const httpStatus =
       exception instanceof HttpException
         ? exception.getStatus()
@@ -28,8 +31,17 @@ export class AllExceptionsFilter implements ExceptionFilter {
     };
     httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
     if (!(exception instanceof HttpException)) {
-      console.log(responseBody);
+      logger.error(responseBody);
       throw exception;
+    } else {
+      logger.error({
+        controller_method: `[${req.url} ${req.method}]`,
+        arguments: {
+          req,
+          res,
+        },
+        errorMessage,
+      });
     }
   }
 }
