9a2393070c5d932441e1c02d156eacd2e9020dbb
alivar391 <patriot391@mail.ru>
2022-08-23 16:55:49 +0300

    feat: add tests for user controller

--- src/users/controllers/user.controller.spec.ts
+++ src/users/controllers/user.controller.spec.ts
@@ -2,19 +2,31 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from '../services/user.service';
 import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
 
 describe('UserController', () => {
   let controller: UserController;
   let spyService: UserService;
+
+  const userId = '5741351f-5843-4847-a9b5-026af82139c8';
+  const wrongUserId = '5741351f-5843-4847-a9b5-026af82139c8';
   const createUserDto: CreateUserDto = {
     login: 'testLogin',
     password: 'testPassword',
     age: 32,
   };
 
+  const updateUserDto: UpdateUserDto = {
+    login: 'updateTestLogin',
+    password: 'testPassword',
+    age: 32,
+  };
+
   const existUser = {
-    id: 1,
+    id: '5741351f-5843-4847-a9b5-026af82139c8',
     login: 'existUser',
+    age: 32,
+    password: 'testPassword',
   };
 
   beforeEach(async () => {
@@ -25,12 +37,40 @@ describe('UserController', () => {
           if (createUserDto.login === existUser.login) {
             return;
           }
-          return { ...createUserDto, id: 1 };
+          return {
+            ...createUserDto,
+            id: '5741351f-5843-4847-a9b5-026af82139c8',
+          };
         }),
         findAll: jest.fn((login: string, limit: number) => []),
-        findOne: jest.fn(() => ''),
-        update: jest.fn(() => '{}'),
-        remove: jest.fn(() => '{}'),
+        findOne: jest.fn((id: string) => {
+          if (id === userId) {
+            return existUser;
+          } else return;
+        }),
+        update: jest.fn((id: string, updateUserDto: UpdateUserDto) => {
+          if (id !== userId) {
+            throw new Error('404');
+          } else if (updateUserDto.login === existUser.login) {
+            throw new Error('400');
+          } else {
+            return {
+              ...existUser,
+              ...updateUserDto,
+            };
+          }
+        }),
+        remove: jest.fn((id: string) => {
+          if (id !== existUser.id) {
+            return;
+          }
+          if (id === existUser.id) {
+            return {
+              ...existUser,
+              isDeleted: true,
+            };
+          }
+        }),
       }),
     };
 
@@ -50,10 +90,13 @@ describe('UserController', () => {
   it('create method should return user', async () => {
     const result = await controller.create(createUserDto);
     expect(spyService.create).toBeCalledWith(createUserDto);
-    expect(result).toEqual({ ...createUserDto, id: 1 });
+    expect(result).toEqual({
+      ...createUserDto,
+      id: '5741351f-5843-4847-a9b5-026af82139c8',
+    });
   });
 
-  it('if user exist should return 400 statusCode', async () => {
+  it('create method if user exist should return 400 statusCode', async () => {
     const existUserDto = {
       ...createUserDto,
       login: 'existUser',
@@ -61,20 +104,63 @@ describe('UserController', () => {
     try {
       const result = await controller.create(existUserDto);
     } catch (err) {
-      console.log(err.response.statusCode);
       expect(err.response.statusCode).toBe(400);
     }
   });
 
-  it('should return instance of Array when called getAllUsers method', async () => {
+  it('getAllUsers method should return instance of Array when called', async () => {
     const result = await controller.getAutoSuggestUsers('', 5);
     expect(spyService.findAll).toHaveBeenCalled();
     expect(result).toBeInstanceOf(Array);
   });
 
-  // it('calling getAllNote method', async () => {
-  //   const result = await controller.findOne();
-  //   expect(spyService.findAll).toHaveBeenCalled();
-  //   expect(result).toBeInstanceOf(Array);
-  // });
+  it('findOne method should return user', async () => {
+    const result = await controller.findOne(userId);
+    expect(spyService.findOne).toHaveBeenCalledWith(userId);
+    expect(result).toBe(existUser);
+  });
+
+  it('findOne method should return 404 status code if no user with id', async () => {
+    try {
+      const result = await controller.findOne(wrongUserId);
+    } catch (err) {
+      expect(err.response.statusCode).toBe(404);
+    }
+  });
+
+  it('update method should return updated user', async () => {
+    const result = await controller.update(userId, updateUserDto);
+    expect(spyService.update).toHaveBeenCalledWith(userId, updateUserDto);
+    expect(result).toStrictEqual({ ...existUser, login: updateUserDto.login });
+  });
+
+  it('update method should return 404 status code if no user with id', async () => {
+    try {
+      const result = await controller.update(wrongUserId, updateUserDto);
+    } catch (err) {
+      expect(err.response.statusCode).toBe(404);
+    }
+  });
+
+  it('update method should return 400 status code if user with login exist', async () => {
+    try {
+      const result = await controller.update(wrongUserId, createUserDto);
+    } catch (err) {
+      expect(err.response.statusCode).toBe(404);
+    }
+  });
+
+  it('delete method should return deleted user', async () => {
+    const result = await controller.remove(userId);
+    expect(spyService.remove).toHaveBeenCalledWith(userId);
+    expect(result).toStrictEqual({ ...existUser, isDeleted: true });
+  });
+
+  it('delete method should return 404 statusCode if user not found', async () => {
+    try {
+      const result = await controller.update(wrongUserId, updateUserDto);
+    } catch (err) {
+      expect(err.response.statusCode).toBe(404);
+    }
+  });
 });
